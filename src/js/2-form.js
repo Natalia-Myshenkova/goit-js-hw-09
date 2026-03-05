5d839143813157a83904cf2cf01bb150c45a7982
Natalia <natamis6122@gmail.com>
2026-03-05 21:44:24 +0200

    add form

---
+++ src/js/2-form.js
@@ -0,0 +1,47 @@
+const storageData = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+let formData = {
+  email: "", 
+  message: "",
+}
+
+const form = document.querySelector('.feedback-form');
+
+const formFieldEmail = form.querySelector('[name="email"]')
+const formFieldMessage = form.querySelector('[name="message"]')
+
+formFieldEmail.value = storageData?.email || ""
+formFieldMessage.value = storageData?.message || ""
+
+
+form.addEventListener('input', event => {
+  formData[event.target.name] = event.target.value.trim();
+
+  localStorage.setItem(
+    'feedback-form-state',
+    JSON.stringify(formData)
+  );
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  if (!formFieldEmail.value.trim() || !formFieldMessage.value.trim()) {
+    alert('Fill please all fields');
+    return;
+  }
+
+  console.log(formData)
+
+  formData = {
+    email: "", 
+    message: "",
+  }
+
+
+  formFieldEmail.value = ""
+  formFieldMessage.value = ""
+
+  localStorage.removeItem('feedback-form-state')
+});
+
